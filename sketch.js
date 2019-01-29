c1cdc5cda5a8858c81428198c10c3ceac60fb232
Phyrik <34688504+Phyrik@users.noreply.github.com>
2019-01-29 18:54:42 +0000

    initial commit for files

---
+++ sketch.js
@@ -0,0 +1,36 @@
+function createPlayersAndCamps() {
+    
+    player2img = loadImage("assets/player2.png");
+    player1jumpedimg = loadImage("assets/player1jumped.png");
+    player2jumpedimg = loadImage("assets/player2jumped.png");
+
+}
+
+function setup() {
+    createCanvas(750, 750);
+    //createPlayersAndCamps();
+    player1img = loadImage("assets/player1.png");
+    image(player1img, width/2, height/2, 0, 0);
+}
+
+function createBoard(size) {
+    for (let i = 1; i < size + 1; i++) {
+        line(width/5*i, 0, 0, width/5*i);
+    }
+    for (let i = 0; i < size + 1; i++) {
+        line(0, height/5*i, width/5*(5-i), height);
+    }
+    for (let i = 1; i < size + 1; i++) {
+        line(width/5*i, height, width, height/5*i);
+    }
+    for (let i = 1; i < size + 1; i++) {
+        line(width, height/5*(5-i), width/5*i, 0);
+    }
+}
+
+function draw() {
+    background(200);
+
+    createBoard(5);
+
+}
