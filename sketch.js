0fccc55365149e044bdd0cf9d4e42870d5bc1524
Phyrik <34688504+Phyrik@users.noreply.github.com>
2019-01-29 19:00:27 +0000

    1

--- sketch.js
+++ sketch.js
@@ -1,17 +1,6 @@
-function createPlayersAndCamps() {
-    
-    player2img = loadImage("assets/player2.png");
-    player1jumpedimg = loadImage("assets/player1jumped.png");
-    player2jumpedimg = loadImage("assets/player2jumped.png");
-
-}
-
 function setup() {
     createCanvas(750, 750);
-    //createPlayersAndCamps();
     createBoard(5);
-    player1img = loadImage("assets/player1.png");
-    image(player1img, width/2, height/2, 0, 0);
 }
 
 function createBoard(size) {
@@ -27,6 +16,14 @@ function createBoard(size) {
     for (let i = 1; i < size + 1; i++) {
         line(width, height/5*(5-i), width/5*i, 0);
     }
+}
+
+function createPlayersAndCamps() {
+    
+    player2img = loadImage("assets/player2.png");
+    player1jumpedimg = loadImage("assets/player1jumped.png");
+    player2jumpedimg = loadImage("assets/player2jumped.png");
+
 }
 
 function draw() {
